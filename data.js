0fd52722ba6eab1fb466f811a4a2644fbd96024b
Desson Medeiros <dessonwms@gmail.com>
2021-02-10 13:15:29 -0400

    Mostrando dados do Back-end no Front-end

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "ZHaokMTWgdY",
+        title: "Formulário animado com JS puro e CSS aanimation | Desson Medeiros",
+        duration: "57 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "RP2twi_n-8c",
+        title: "Formulário animado com JS puro e CSS aanimation | Desson Medeiros",
+        duration: "33 min",
+        price: "Free"
+    },
+    {
+        id: "kzbzBqfdu0U",
+        title: "Formulário animado com JS puro e CSS aanimation | Desson Medeiros",
+        duration: "22 min",
+        price: "Free"
+    },
+    {
+        id: "ZHaokMTWgdY",
+        title: "Formulário animado com JS puro e CSS aanimation | Desson Medeiros",
+        duration: "10 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "RP2twi_n-8c",
+        title: "Formulário animado com JS puro e CSS aanimation | Desson Medeiros",
+        duration: "15 min",
+        price: "Free"
+    },
+    {
+        id: "kzbzBqfdu0U",
+        title: "Formulário animado com JS puro e CSS aanimation | Desson Medeiros",
+        duration: "23 min",
+        price: "Free",
+        featured: true
+    }
+]
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+const videos = require("./data")
+
+// Inclusão dos arquivos estáticos
+server.use(express.static('public'))
+
+// Configuração da Template Engine
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
 
 server.get("/", function(req, res) {
-    return res.send("Agora sim")
+
+    const about = {
+        avatar_url: "https://avatars.githubusercontent.com/u/61638378?s=460&u=fba73b95ced37326d61da475e422fa9c15e3b4f0&v=4",
+        name: "Desson Medeiros",
+        role: "Instrutor - Lynx Code",
+        description: 'Programador full-stack, focado em trazer conteúdo de qualidade para iniciantes em programação. Colaborador <a href="https://www.lynxcode.com.br" target="_blank">Lynx Code</a>',
+        links: [
+            { name: "Github", url: "https://github.com/dessonwms" },
+            { name: "Twitter", url: "https://www.twitter.com/dessonwms" },
+            { name: "Linkdin", url: "https://www.linkdin.com" }
+        ]
+    }
+
+    return res.render("about", { about })
 })
+
+server.get("/portfolio", function(req, res) {
+    return res.render("portfolio", { items: videos })
+})
+
+
 
 server.listen(5000, function() {
     console.log("Server is running")
